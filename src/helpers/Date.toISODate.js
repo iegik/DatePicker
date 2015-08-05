29d0ab5c0cd585946eee36cf56d3195ef03954cb
Artrs Jansons <arturs.jansons05@latvenergo.lv>
2015-08-05 11:42:02 +0300

    Add helper Date.toISODate

--- src/helpers/Date.toISODate.js
+++ src/helpers/Date.toISODate.js
@@ -1,3 +1,9 @@
+if (!Date.prototype.format) {
+    (function () {
+        Date.prototype.format = require('./Date.format.js');
+    }());
+}
+
 module.exports = function () {
     return this.format('y-M-d');
 };
